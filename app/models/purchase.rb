e665319df84044030822aa4cb4c555d24a37bb9e
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-08-04 20:31:52 +0200

    swedish interface.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,9 @@
 class Purchase < ActiveRecord::Base
   validates_presence_of :purchaser, :purchase_date
   validates_numericality_of :amount
+
+  def purchase_date_formatted
+    purchase_date.strftime("%Y-%m-%d")
+  end
+
 end
